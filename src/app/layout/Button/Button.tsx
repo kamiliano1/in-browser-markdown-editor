e00c6c5d6ed1142faa235becec40a3e79d1cea36
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-11-25 23:55:19 +0100

    Initial commit

---
+++ src/app/layout/Button/Button.tsx
@@ -0,0 +1,25 @@
+import React, { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+
+type ButtonProps<T extends ElementType> = {
+  renderAs?: T;
+  children: ReactNode;
+  cssClassName?: string;
+} & ComponentPropsWithoutRef<T>;
+
+const Button = <T extends ElementType = "button">({
+  renderAs,
+  children,
+  cssClassName,
+  ...rest
+}: ButtonProps<T>): JSX.Element => {
+  return (
+    <button
+      {...rest}
+      className={`${cssClassName} bg-orange hover:bg-orangeHover py-[9px] px-4
+       text-100 rounded text-headingM font-roboto`}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,120 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      roboto: "var(--font-roboto)",
+      robotoSlab: "var(--font-roboto-slab)",
+      robotoMono: "var(--font-roboto-mono)",
     },
+    colors: {
+      1000: "hsl(225, 9%, 9%)",
+      900: "hsl(216, 8%, 12%)",
+      800: "hsl(220, 7%, 18%)",
+      700: "hsl(216, 9%, 23%)",
+      600: "hsl(216, 8%, 38%)",
+      500: "hsl(213, 4%, 51%)",
+      400: "hsl(222, 9%, 78%)",
+      300: "hsl(0, 0%, 89%)",
+      200: "hsl(0, 0%, 96%)",
+      100: "hsl(0, 0%, 100%)",
+      orange: "hsl(13, 75%, 58%)",
+      orangeHover: "hsl(21, 86%, 67%)",
+    },
+    fontSize: {
+      headingM: [
+        ".9375rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "400",
+        },
+      ],
+      headingS: [
+        ".8725rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "500",
+          letterSpacing: "2px",
+        },
+      ],
+      bodyM: [
+        ".8125rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "300",
+        },
+      ],
+      previewH1: [
+        "2rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "700",
+        },
+      ],
+      previewH2: [
+        "1.75rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "300",
+        },
+      ],
+      previewH3: [
+        "1.5rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "700",
+        },
+      ],
+      previewH4: [
+        "1.25rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "700",
+        },
+      ],
+      previewH5: [
+        "1rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "700",
+        },
+      ],
+      previewH6: [
+        ".875rem",
+        {
+          // lineHeight: "normal",
+          fontWeight: "700",
+        },
+      ],
+      previewParagraph: [
+        ".875rem",
+        {
+          lineHeight: "171.429%",
+          fontWeight: "400",
+        },
+      ],
+      previewParagraphBold: [
+        ".875rem",
+        {
+          lineHeight: "171.429%",
+          fontWeight: "700",
+        },
+      ],
+      markdown: [
+        ".875rem",
+        {
+          lineHeight: "171.429%",
+          fontWeight: "400",
+        },
+      ],
+    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
