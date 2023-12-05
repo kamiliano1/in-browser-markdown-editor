8f900926a7c28235c9ad35a294a0eb39afba0d28
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-12-05 21:37:00 +0100

    Started MarkdownEditor

--- src/layout/MarkdownEditors/MarkdownPreview.tsx
+++ src/layout/MarkdownEditors/MarkdownPreview.tsx
@@ -18,6 +18,7 @@ const MarkdownPreview: React.FC<MarkdownPreviewProps> = ({}) => {
       )[0].content
     );
     setPrintedMarkdown(activatedMarkdownContent.split("\n"));
+    setPrintedMarkdown((prev) => prev.filter((item) => item !== ""));
   }, [
     activatedMarkdownContent,
     markdownEditorState.activeMarkdownId,
@@ -37,6 +38,119 @@ const MarkdownPreview: React.FC<MarkdownPreviewProps> = ({}) => {
     console.log(activatedMarkdownContent.split("\n"));
   };
 
+  // type MarkdownType = {
+  //   item: string;
+  //   id: number;
+  //   startWith: string;
+  //   font: string;
+  //   HtmlElement: string;
+  // };
+
+  // const markdownUtility = ({
+  //   item,
+  //   id,
+  //   startWith,
+  //   font,
+  //   HtmlElement,
+  // }: MarkdownType) => {
+  //   if (item.startsWith(startWith)) {
+  //     return (
+  //       <HtmlElement
+  //         key={id}
+  //         className={`${font}  ${
+  //           !markdownEditorState.isLightMode ? "text-100" : "text-700"
+  //         }`}
+  //       >
+  //         {item.slice(startWith.length)}
+  //       </HtmlElement>
+  //     );
+  //   }
+  // };
+
+  const MarkdownPrint = () => {
+    return printedMarkdown.map((item, id) => {
+      // markdownUtility({
+      //   item: item,
+      //   id: id,
+      //   startWith: "# ",
+      //   font: "text-previewH1",
+      //   HtmlElement: "h1",
+      // });
+      if (item.startsWith("# ")) {
+        return (
+          <h1
+            key={id}
+            className={`text-previewH1  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            {item.slice(2)}
+          </h1>
+        );
+      }
+      if (item.startsWith("## ")) {
+        return (
+          <h2
+            key={id}
+            className={`text-previewH2  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            {item.slice(3)}
+          </h2>
+        );
+      }
+      if (item.startsWith("### ")) {
+        return (
+          <h3
+            key={id}
+            className={`text-previewH3  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            {item.slice(4)}
+          </h3>
+        );
+      }
+      if (item.startsWith("#### ")) {
+        return (
+          <h4
+            key={id}
+            className={`text-previewH4  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            {item.slice(5)}
+          </h4>
+        );
+      }
+      if (item.startsWith("##### ")) {
+        return (
+          <h5
+            key={id}
+            className={`text-previewH5  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            {item.slice(6)}
+          </h5>
+        );
+      }
+      if (item.startsWith("###### ")) {
+        return (
+          <h5
+            key={id}
+            className={`text-previewH6  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            {item.slice(7)}
+          </h5>
+        );
+      }
+      return <p key={id}>{item}</p>;
+    });
+  };
   return (
     <div className="w-full">
       <button onClick={sprawdz}>Sprawdz</button>
@@ -86,11 +200,62 @@ const MarkdownPreview: React.FC<MarkdownPreviewProps> = ({}) => {
             : "bg-100 text-700"
         }`}
       >
-        <div className="max-w-[981px] mx-auto">
-          {printedMarkdown.map((item, id) => {
-            if (item === "") return <br></br>;
-            return <p key={id}>{item}</p>;
-          })}
+        <div className="max-w-[981px] mx-auto markdownPrinter flex flex-col gap-5">
+          <MarkdownPrint />
+          <h1
+            className={`text-previewH1  ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            deserunt molestias. Iusto ratione sequi debitis qui adipisci dicta
+            vel autem.
+          </h1>
+          <h2
+            className={`text-previewH2 ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            deserunt molestias. Iusto ratione sequi debitis qui adipisci dicta
+            vel autem.
+          </h2>
+          <h3
+            className={`text-previewH3 ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            deserunt molestias. Iusto ratione sequi debitis qui adipisci dicta
+            vel autem.
+          </h3>
+          <h4
+            className={`text-previewH4 ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            deserunt molestias. Iusto ratione sequi debitis qui adipisci dicta
+            vel autem.
+          </h4>
+          <h5
+            className={`text-previewH5 ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            deserunt molestias. Iusto ratione sequi debitis qui adipisci dicta
+            vel autem.
+          </h5>
+          <h6
+            className={`text-previewH6 ${
+              !markdownEditorState.isLightMode ? "text-100" : "text-700"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+            deserunt molestias. Iusto ratione sequi debitis qui adipisci dicta
+            vel autem.
+          </h6>
         </div>
       </div>
     </div>
